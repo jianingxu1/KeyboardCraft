7abf90862af55a090a964c96461aaede5123a388
muhammad.yasin.khokhar <muhammad.yasin.khokhar@estudiantat.upc.edu>
2023-11-11 23:31:26 +0100

    Add unit test for Texto

---
+++ src/test/java/edu.upc.prop.cluster23/domain/TestTexto.java
@@ -0,0 +1,40 @@
+package edu.upc.prop.cluster23.domain;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import edu.upc.prop.cluster23.domain.Texto;
+
+public class TestTexto{
+  
+  @Test
+  public void testDefaultConstructor() {
+      Texto texto = new Texto();
+      assertEquals("", texto.getTexto());
+  }
+  
+  @Test
+  public void testParametrizedConstructor() {
+    String input = "abcdedfghijklmnopqrstuvwxyz";
+    Texto texto = new Texto(input);
+    assertEquals(input, texto.getTexto());
+  }
+  
+  @Test
+  public void testModificarTexto() {
+    String input = "abcdedfghijklmnopqrstuvwxyz";
+    Texto texto = new Texto(input);
+    assertEquals(input, texto.getTexto());
+    String input2 = "abcdef";
+    texto.modificarTexto(input2);
+    assertEquals(input2, texto.getTexto());
+  }
+  
+  @Test
+  public void testGetTexto() {
+    String input = "abcdedfghijklmnopqrstuvwxyz";
+    Texto texto = new Texto(input);
+    assertEquals(input, texto.getTexto());
+  }
+
+}
