9c700733175a79447e3ccd4ec6ee8f570ebd48d4
ruben.catalan <ruben.catalan@estudiantat.upc.edu>
2023-11-15 14:08:06 +0100

    Update Tview

--- src/main/java/edu/upc/prop/cluster23/presentation/Tview.java
+++ src/main/java/edu/upc/prop/cluster23/presentation/Tview.java
@@ -2,18 +2,34 @@ package edu.upc.prop.cluster23.presentation;
 
 import edu.upc.prop.cluster23.domaincontrollers.CtrlDominio;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
+
+/**
+ * Clase Tview
+ * Vista por terminal del programa
+ * @author Rubén Catalán Rua (ruben.catalan@estudiantat.upc.edu)
+ */
 
 public class Tview {
 
     private CtrlDominio controladorDominio;
     private Scanner input;
 
+    /**
+     * Constructora
+     */
+
     public Tview(){
         controladorDominio = new CtrlDominio();
         input = new Scanner(System.in);
     }
+
+    /**
+     * Inicializa el controlador de dominio y llama a la función que ejecuta los comandos
+     */
 
     public void init() {
         controladorDominio.inicializarCtrlDominio();
@@ -21,7 +37,7 @@ public class Tview {
         run();
     }
 
-    public void run() throws IllegalStateException {
+    public void run() {
 
         int value = -1;
 
@@ -35,7 +51,7 @@ public class Tview {
             switch (value) {
                 case 1:
                     System.out.println("TECLADO:");
-                    System.out.println("1. Crear\n2. Modificar\n3. Borrar");
+                    System.out.println("1. Crear\n2. Modificar\n3. Borrar\n4. Mostrar\n5. Atrás");
 
                     int opt = input.nextInt();
 
@@ -45,17 +61,39 @@ public class Tview {
                             break;
 
                         case 2:
-                            // code
+                            System.out.println("1. Swap de teclas\n2. Cambiar algoritmo" +
+                                    "(NO IMPLEMENTADO EN ESTA PRIMERA ENTREGA)\n" +
+                                    "3. Cambiar texto\n 4. Cambiar lista de palabras");
                             break;
 
                         case 3:
                             FuncBorrarTeclado();
+                            break;
+
+                    case 4:
+                            FuncMostrarTeclado();
                             break;
                     }
                     break;
                 case 2:
                     System.out.println("ALFABETO:");
                     System.out.println("1. Crear\n2. Modificar\n3. Borrar");
+
+                    int optTec = input.nextInt();
+
+                    switch (optTec) {
+                    case 1:
+                        FuncCreacionAlfabeto();
+                        break;
+
+                    case 2:
+                        FuncModificarAlfabeto();
+                        break;
+
+                    case 3:
+                        FuncBorrarAlfabeto();
+                        break;
+                    }
                     break;
 
                 case 3:
@@ -103,5 +141,50 @@ public class Tview {
             controladorDominio.borrarTeclado(nameDel);
             System.out.println("El teclado " + nameDel + " ha sido borrado");
         }
+    }
+
+    void FuncMostrarTeclado() {
+        System.out.println("Introduce el nombre del teclado a mostrar:");
+        input.nextLine();
+
+        String name = input.nextLine();
+
+        String keyboard = controladorDominio.consultarDistribucionDeTeclado(name);
+
+        System.out.println(keyboard);
+    }
+
+    void FuncCreacionAlfabeto() {
+
+        System.out.println("Escribe el nombre y los caractéres del alfabeto a crear");
+
+        input.nextLine();
+
+        String name = input.nextLine();
+        String idAlf = input.nextLine();
+
+        controladorDominio.añadirAlfabeto(name,idAlf);
+    }
+
+    void FuncModificarAlfabeto() {
+
+        System.out.println("Escribe el nombre y los caractéres del alfabeto a modificar");
+
+        input.nextLine();
+
+        String name = input.nextLine();
+        String idAlf = input.nextLine();
+
+        controladorDominio.modificarAlfabeto(name,idAlf);
+    }
+
+    void FuncBorrarAlfabeto() {
+        System.out.println("Escribe el nombre del alfabeto a borrar");
+
+        input.nextLine();
+
+        String name = input.nextLine();
+
+        controladorDominio.eliminarAlfabeto(name);
     }
 }
