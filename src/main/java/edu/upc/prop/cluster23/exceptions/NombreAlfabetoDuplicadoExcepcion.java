a5ebad905e0ef17b0058853311bbbccd92fb3deb
momin.miah <momin.miah@estudiantat.upc.edu>
2023-11-18 18:38:16 +0100

    Move Excpecion Classes to Parent Folder

---
+++ src/main/java/edu/upc/prop/cluster23/exceptions/NombreAlfabetoDuplicadoExcepcion.java
@@ -0,0 +1,12 @@
+package edu.upc.prop.cluster23.exceptions;
+
+/** Clase NombreAlfabetoDuplicadoExcepcion
+ *  Representa una excepción que se lanza cuando se intenta crear un alfabeto con un nombre que ya existe en el sistema.
+ *  @author Muhammad Yasin Khokhar (muhammad.yasin.khokhar@estudiantat.upc.edu)
+ *  @co-author Momin Miah Begum (momin.miah@estudiantat.upc.edu)
+ */
+public class NombreAlfabetoDuplicadoExcepcion extends Exception {
+    public NombreAlfabetoDuplicadoExcepcion(String errorMessage) {
+        super("El alfabeto " + errorMessage + " ya existe en el conjunto de alfabetos.");
+    }
+}
