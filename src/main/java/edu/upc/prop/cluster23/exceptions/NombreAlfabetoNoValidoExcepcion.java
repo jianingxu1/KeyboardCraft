35cf5c3347f1a999a376cfebaf7d575a8753d51c
muhammad.yasin.khokhar <muhammad.yasin.khokhar@estudiantat.upc.edu>
2023-11-19 02:22:09 +0100

    Add new exceptions NombreAlfabetoNoValido and NombreTecladoNovalido

---
+++ src/main/java/edu/upc/prop/cluster23/exceptions/NombreAlfabetoNoValidoExcepcion.java
@@ -0,0 +1,12 @@
+package edu.upc.prop.cluster23.exceptions;
+
+/** Clase NombreAlfabetoNoExisteExcepcion
+ * Representa una excepción que se lanza cuando se intenta acceder a un alfabeto que no existe en el sistema.
+ *  @author Muhammad Yasin Khokhar (muhammad.yasin.khokhar@estudiantat.upc.edu)
+ *  @co-author Momin Miah Begum (momin.miah@estudiantat.upc.edu)
+ */
+public class NombreAlfabetoNoValidoExcepcion extends Exception {
+    public NombreAlfabetoNoValidoExcepcion(String errorMessage) {
+        super(errorMessage);
+    }
+}
