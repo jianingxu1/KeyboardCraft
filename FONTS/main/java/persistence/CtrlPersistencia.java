2dba4f52a07f33335404fc3886e47657f421037d
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2023-12-11 19:16:21 +0100

    Delete unnecessary exceptions in CtrlPersistencia

--- FONTS/main/java/persistence/CtrlPersistencia.java
+++ FONTS/main/java/persistence/CtrlPersistencia.java
@@ -12,61 +12,37 @@ public class CtrlPersistencia {
     private CtrlFicherosAlfabetos ctrlFicherosAlfabetos;
 
     public CtrlPersistencia() {
-        // Constructor vacío
     }
 
     public void guardarUsuarios(CjtUsuarios cjtUsuarios) throws EscrituraIncorrectaFicheroExcepcion {
-        try {
-            ctrlFicherosUsuarios = new CtrlFicherosUsuarios();
-            ctrlFicherosUsuarios.guardarUsuarios(cjtUsuarios);
-        } catch (Exception e) {
-            throw new EscrituraIncorrectaFicheroExcepcion(e.getMessage());
-        }
+        ctrlFicherosUsuarios = new CtrlFicherosUsuarios();
+        ctrlFicherosUsuarios.guardarUsuarios(cjtUsuarios);
     }
 
-    public void guardarAlfabetos(CjtAlfabetos cjtAlfabetos, String userName) throws EscrituraIncorrectaFicheroExcepcion {
-        try {
-            ctrlFicherosAlfabetos = new CtrlFicherosAlfabetos();
-            ctrlFicherosAlfabetos.guardarAlfabetos(cjtAlfabetos, userName);
-        } catch (Exception e) {
-            throw new EscrituraIncorrectaFicheroExcepcion(e.getMessage());
-        }
+    public void guardarAlfabetos(CjtAlfabetos cjtAlfabetos, String userName)
+            throws EscrituraIncorrectaFicheroExcepcion {
+        ctrlFicherosAlfabetos = new CtrlFicherosAlfabetos();
+        ctrlFicherosAlfabetos.guardarAlfabetos(cjtAlfabetos, userName);
     }
 
     public void guardarTeclados(CjtTeclados cjtTeclados, String userName) throws EscrituraIncorrectaFicheroExcepcion {
-        try {
-            ctrlFicherosTeclados = new CtrlFicherosTeclados();
-            ctrlFicherosTeclados.guardarTeclados(cjtTeclados, userName);
-        } catch (Exception e) {
-            throw new EscrituraIncorrectaFicheroExcepcion(e.getMessage());
-        }
+        ctrlFicherosTeclados = new CtrlFicherosTeclados();
+        ctrlFicherosTeclados.guardarTeclados(cjtTeclados, userName);
     }
 
     public CjtUsuarios cargarUsuarios() throws LecturaIncorrectaFicheroExcepcion {
-        try {
-            ctrlFicherosUsuarios = new CtrlFicherosUsuarios();
-            return ctrlFicherosUsuarios.cargarUsuarios();
-        } catch (Exception e) {
-            throw new LecturaIncorrectaFicheroExcepcion(e.getMessage());
-        }
+        ctrlFicherosUsuarios = new CtrlFicherosUsuarios();
+        return ctrlFicherosUsuarios.cargarUsuarios();
     }
 
     public CjtAlfabetos cargarAlfabetos(String userName) throws LecturaIncorrectaFicheroExcepcion {
-        try {
-            ctrlFicherosAlfabetos = new CtrlFicherosAlfabetos();
-            return ctrlFicherosAlfabetos.cargarAlfabetos(userName);
-        } catch (Exception e) {
-            throw new LecturaIncorrectaFicheroExcepcion(e.getMessage());
-        }
+        ctrlFicherosAlfabetos = new CtrlFicherosAlfabetos();
+        return ctrlFicherosAlfabetos.cargarAlfabetos(userName);
     }
 
     public CjtTeclados cargarTeclados(String userName) throws LecturaIncorrectaFicheroExcepcion {
-        try {
-            ctrlFicherosTeclados = new CtrlFicherosTeclados();
-            return ctrlFicherosTeclados.cargarTeclados(userName);
-        } catch (Exception e) {
-            throw new LecturaIncorrectaFicheroExcepcion(e.getMessage());
-        }
+        ctrlFicherosTeclados = new CtrlFicherosTeclados();
+        return ctrlFicherosTeclados.cargarTeclados(userName);
     }
 
     public void eliminarUsuario(String nombreUsuario) {
