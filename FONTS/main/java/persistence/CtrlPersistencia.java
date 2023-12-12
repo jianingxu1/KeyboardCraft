d8b12a1d82bd794a3b77a31ad3ca3ba4986e3c0d
muhammad.yasin.khokhar <muhammad.yasin.khokhar@estudiantat.upc.edu>
2023-12-12 15:44:01 +0100

    Revert "Revert "Rename & Update Persistence Classes""
    This reverts commit 039d68da8710a7ede19b2acf40b56703521c6c78.

--- FONTS/main/java/persistence/CtrlPersistencia.java
+++ FONTS/main/java/persistence/CtrlPersistencia.java
@@ -7,46 +7,92 @@ import exceptions.*;
 
 public class CtrlPersistencia {
 
-    private CtrlFicherosTeclados ctrlFicherosTeclados;
-    private CtrlFicherosUsuarios ctrlFicherosUsuarios;
-    private CtrlFicherosAlfabetos ctrlFicherosAlfabetos;
+    
+    private GestorFicherosTeclados gestorFicherosTeclados;
+    private GestorFicherosUsuarios gestorFicherosUsuarios;
+    private GestorFicherosAlfabetos gestorFicherosAlfabetos;
 
+
+    /*
+     * Constructora de la clase CtrlPersistencia
+     */
     public CtrlPersistencia() {
     }
 
+    /*
+     * Guarda los usuarios en el fichero de usuarios
+     * @throws EscrituraIncorrectaFicheroExcepcion Si el fichero de usuarios no se ha podido escribir correctamente
+     * @param cjtUsuarios El conjunto de usuarios que se quiere guardar
+     */
     public void guardarUsuarios(CjtUsuarios cjtUsuarios) throws EscrituraIncorrectaFicheroExcepcion {
-        ctrlFicherosUsuarios = new CtrlFicherosUsuarios();
-        ctrlFicherosUsuarios.guardarUsuarios(cjtUsuarios);
+        gestorFicherosUsuarios = new GestorFicherosUsuarios();
+        gestorFicherosUsuarios.guardarUsuarios(cjtUsuarios);
     }
 
+    /*
+     * Guarda los alfabetos en el fichero de alfabetos
+     * @throws EscrituraIncorrectaFicheroExcepcion Si el fichero de alfabetos no se ha podido escribir correctamente
+     * @param cjtAlfabetos El conjunto de alfabetos que se quiere guardar
+     */
     public void guardarAlfabetos(CjtAlfabetos cjtAlfabetos, String userName)
             throws EscrituraIncorrectaFicheroExcepcion {
-        ctrlFicherosAlfabetos = new CtrlFicherosAlfabetos();
-        ctrlFicherosAlfabetos.guardarAlfabetos(cjtAlfabetos, userName);
+        gestorFicherosAlfabetos = new GestorFicherosAlfabetos();
+        gestorFicherosAlfabetos.guardarAlfabetos(cjtAlfabetos, userName);
     }
 
+    /*
+     * Guarda los teclados en el fichero de teclados
+     * @throws EscrituraIncorrectaFicheroExcepcion Si el fichero de teclados no se ha podido escribir correctamente
+     * @param cjtTeclados El conjunto de teclados que se quiere guardar
+     * @param userName El nombre del usuario que quiere guardar los teclados
+     */
     public void guardarTeclados(CjtTeclados cjtTeclados, String userName) throws EscrituraIncorrectaFicheroExcepcion {
-        ctrlFicherosTeclados = new CtrlFicherosTeclados();
-        ctrlFicherosTeclados.guardarTeclados(cjtTeclados, userName);
+        gestorFicherosTeclados = new GestorFicherosTeclados();
+        gestorFicherosTeclados.guardarTeclados(cjtTeclados, userName);
     }
 
+    /*
+     * Carga los usuarios del fichero de usuarios
+     * @return El conjunto de usuarios que se ha cargado
+     * @throws LecturaIncorrectaFicheroExcepcion Si el fichero de usuarios no se ha podido leer correctamente
+     */
     public CjtUsuarios cargarUsuarios() throws LecturaIncorrectaFicheroExcepcion {
-        ctrlFicherosUsuarios = new CtrlFicherosUsuarios();
-        return ctrlFicherosUsuarios.cargarUsuarios();
+        gestorFicherosUsuarios = new GestorFicherosUsuarios();
+        return gestorFicherosUsuarios.cargarUsuarios();
     }
 
+    /*
+     * Carga los alfabetos del fichero de alfabetos
+     * @return El conjunto de alfabetos que se ha cargado
+     * @throws LecturaIncorrectaFicheroExcepcion Si el fichero de alfabetos no se ha podido leer correctamente
+     * @param userName El nombre del usuario que quiere cargar los alfabetos
+     */
     public CjtAlfabetos cargarAlfabetos(String userName) throws LecturaIncorrectaFicheroExcepcion {
-        ctrlFicherosAlfabetos = new CtrlFicherosAlfabetos();
-        return ctrlFicherosAlfabetos.cargarAlfabetos(userName);
+        gestorFicherosAlfabetos = new GestorFicherosAlfabetos();
+        return gestorFicherosAlfabetos.cargarAlfabetos(userName);
     }
 
+    /*
+     * Carga los teclados del fichero de teclados
+     * @return El conjunto de teclados que se ha cargado
+     * @throws LecturaIncorrectaFicheroExcepcion Si el fichero de teclados no se ha podido leer correctamente
+     * @param userName El nombre del usuario que quiere cargar los teclados
+     */
     public CjtTeclados cargarTeclados(String userName) throws LecturaIncorrectaFicheroExcepcion {
-        ctrlFicherosTeclados = new CtrlFicherosTeclados();
-        return ctrlFicherosTeclados.cargarTeclados(userName);
+        gestorFicherosTeclados = new GestorFicherosTeclados();
+        return gestorFicherosTeclados.cargarTeclados(userName);
     }
 
+    /*
+     * Elimina el usuario del fichero de usuarios
+     * @param nombreUsuario El nombre del usuario que se quiere eliminar
+     */
     public void eliminarUsuario(String nombreUsuario) {
-        ctrlFicherosUsuarios = new CtrlFicherosUsuarios();
-        ctrlFicherosUsuarios.eliminarUsuario(nombreUsuario);
+        gestorFicherosTeclados = new GestorFicherosTeclados();
+        gestorFicherosTeclados.eliminarTecladosDeUsuario(nombreUsuario);
+
+        gestorFicherosAlfabetos = new GestorFicherosAlfabetos();
+        gestorFicherosAlfabetos.eliminarAlfabetosDeUsuario(nombreUsuario);
+
     }
 }
--- FONTS/main/java/persistence/GestorFicherosTeclados.java
+++ FONTS/main/java/persistence/GestorFicherosTeclados.java
@@ -6,8 +6,21 @@ import exceptions.LecturaIncorrectaFicheroExcepcion;
 
 import java.io.*;
 
-public class CtrlFicherosTeclados {
+public class GestorFicherosTeclados {
 
+    /*
+     * Constructora de la clase CtrlFicherosTeclados
+     */
+
+    public GestorFicherosTeclados() {
+    }
+
+    /*
+     * Guarda los teclados en el fichero de teclados
+     * @throws EscrituraIncorrectaFicheroExcepcion Si el fichero de teclados no se ha podido escribir correctamente
+     * @param cjtTeclados El conjunto de teclados que se quiere guardar
+     * @param userName El nombre del usuario que quiere guardar los teclados
+     */
     public void guardarTeclados(CjtTeclados cjtTeclados, String userName) throws EscrituraIncorrectaFicheroExcepcion {
         if (userName.trim().isEmpty()) return;
         try {
@@ -22,6 +35,12 @@ public class CtrlFicherosTeclados {
         }
     }
 
+    /*
+     * Carga los teclados del fichero de teclados
+     * @return El conjunto de teclados que se ha cargado
+     * @throws LecturaIncorrectaFicheroExcepcion Si el fichero de teclados no se ha podido leer correctamente
+     * @param userName El nombre del usuario que quiere cargar los teclados
+     */
     public CjtTeclados cargarTeclados(String userName) throws LecturaIncorrectaFicheroExcepcion {
         CjtTeclados cjtTeclados = CjtTeclados.getInstance();
         String path = "../DATA/" + userName + "Teclados" + ".prop";
@@ -46,6 +65,20 @@ public class CtrlFicherosTeclados {
         return cjtTeclados;
     }
 
+    /*
+     * Elimina el fichero de teclados de un usuario
+     * @param userName El nombre del usuario que quiere eliminar sus teclados
+     */
+    public void eliminarTecladosDeUsuario(String userName) {
+        File fileTeclados = new File("../DATA/" + userName + "Teclados" + ".prop");
+        fileTeclados.delete();
+    }
+
+    /*
+     * Convierte un string en una matriz de caracteres
+     * @param distribucionString El string que contiene la distribucion
+     * @return La matriz de caracteres que contiene la distribucion
+     */
     private static char[][] convertirStringADistribucion(String distribucionString) {
         char[][] distribucion = new char[3][10];
         int i = 0;
