dceb8f4a88ea1bd93925b72dd5e67e1e5a97c746
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2023-12-22 17:35:34 +0100

    Add javadoc for Algoritmo

--- FONTS/main/java/domain/Algoritmo.java
+++ FONTS/main/java/domain/Algoritmo.java
@@ -3,10 +3,21 @@ package domain;
 import java.util.Map;
 
 /**
- * Interface Algoritmo
+ * Interfaz Algoritmo
  * 
- * @author Rubén Catalán Rua (ruben.catalan@estudiantat.upc.edu)
+ * Esta interfaz define el contrato que deben seguir todos los algoritmos de generación de distribuciones.
+ * Los algoritmos implementarán el método generarDistribucion para crear una matriz de caracteres basada en
+ * el alfabeto y los bigramas con frecuencia proporcionados.
+ * 
+ * @author Ruben Catalan Rua (ruben.catalan@estudiantat.upc.edu)
  */
 public interface Algoritmo {
+    /**
+     * Genera una distribución de caracteres.
+     * 
+     * @param alfabeto El alfabeto sobre el cual se generará la distribución.
+     * @param bigramasConFrecuencia Un mapa que contiene bigramas y sus frecuencias.
+     * @return Una matriz de caracteres que representa la distribución generada.
+     */
     public Character[][] generarDistribucion(Alfabeto alfabeto, Map<String, Integer> bigramasConFrecuencia);
 }
